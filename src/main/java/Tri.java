47427131639d26ebdfbc4a7a7c7ee8948011812f
DorianMadi <dorianmadicontact@gmail.com>
2015-11-10 10:43:38 +0100

    classe du Tri

---
+++ src/main/java/Tri.java
@@ -0,0 +1,22 @@
+
+public class Tri {
+	public static void triInsertionTableau(int[] tab){ //tri par insertion
+		int n=tab.length;
+		int j;
+		for(int i=1; i<n; i++){
+			int flag=tab[i];
+			for(j=i; j>0 && tab[j-1]>flag; j--){
+				tab[j]=tab[j-1];
+			}
+			tab[j]=flag;
+		}
+	}
+	public static void main(String[] args){
+		int[] tab={2529,58921,59283,41587,5896,0,8};
+		triInsertionTableau(tab);
+		System.out.print("|");
+		for(int i=0; i<tab.length; i++){
+			System.out.print(tab[i]+"|");
+		}
+	}
+}
