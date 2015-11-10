96a863c20a191d7e190bc6307edc016ba4072176
SoLRB <sophiane.laroub@gmail.com>
2015-11-10 10:26:00 +0100

    voici mon fichier

--- src/main/java/Recherche.java
+++ src/main/java/Recherche.java
@@ -7,17 +7,25 @@ public class Recherche{
 	
 	
 	
-	public void main(){
+	public void rechercheDansTableau(int tab[], int n){
 		
-		
-		
-		
+			int i=0;
+			if(n < tab[tab.length/2])
+				for(i=0;i<tab.length/2; i++){
+				if(tab[i] == n)
+					System.out.println("L'occurence " + n + " existe à la position " + i);
+				}
+			
+			if(n > tab[tab.length/2])
+				for(i=tab.length/2;i<tab.length; i++){
+				if(tab[i] == n)
+					System.out.println("L'occurence " + n + " existe à la position " + i);
+				}	
 		
 		
 		
 		
 	}
-	
 	
 	
 	
