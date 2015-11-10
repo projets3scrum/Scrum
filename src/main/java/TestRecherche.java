bbc18f6c0836af7de39bed8aa4104949a7a12a98
SoLRB <sophiane.laroub@gmail.com>
2015-11-10 10:34:10 +0100

    voici la recherche

---
+++ src/main/java/TestRecherche.java
@@ -0,0 +1,18 @@
+
+
+public class TestRecherche {
+	public void main(){
+		int tab[]={4,4,7,8,9};
+		
+		int n = 4;
+		Recherche r=new Recherche();
+		r.rechercheDansTableau(tab,n);
+		
+		
+		
+		
+		
+		
+	}
+
+}
