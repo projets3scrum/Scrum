792c2e00295b882088c420e638c4a294e2f41117
DorianMadi <dorianmadicontact@gmail.com>
2015-11-17 11:30:55 +0100

    tests jacoco et findbugs(problème réglé)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 	
 		int tabEx[] = {1,63,15,9,-11,11,14,0,5} ; 
-		int str ;
-		String sortie ; 
 		Tri.triInsertionTableau(tabEx); 
-		do
-		{	Scanner sc = new Scanner(System.in);
-			System.out.println("Saisir le nombre que vous recherchez : \n");
-			str = sc.nextInt();
-			Recherche.rechercheDansTableau(tabEx, str ) ;
-			
-			Scanner sc2 = new Scanner(System.in);
-			System.out.println("Voulez vous partir ? O/N \n");
-			sortie = sc2.nextLine();
-			
-			
-		}while (!sortie.equals("o")&&!sortie.equals("O")); 
-		
+		Recherche.rechercheDansTableau(tabEx, 9);
+		Recherche.rechercheDansTableau(tabEx, 17);
+		Recherche.rechercheDansTableau(tabEx, -5);	
 	}
 
 }
